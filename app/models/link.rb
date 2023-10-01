6f5d273944904c77214a6583d306cca1c0183e74
beckpaul <beckrpaul@gmail.com>
2023-10-01 18:14:28 -0400

    meta data addtions - clean up spelling issues

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,6 +4,10 @@ class Link < ApplicationRecord
 
   scope :recent_first, -> {order(created_at: :desc) }
 
+  after_save_commit if: :url_previously_changed? do
+    MetadataJob.perform_later(to_param)
+  end
+
 
   def to_param
     ShortCode.encode(id)
